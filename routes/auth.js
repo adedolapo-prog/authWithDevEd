d2312128b605772bf7458e3ce62011f36e8d80db
adedolapo <shayne.doly94@gmail.com>
2021-06-23 10:21:46 +0100

    adjusts slight issue with the joi user validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,24 +1,21 @@
 const router = require("express").Router()
 const User = require("../models/userModel")
-const schema = require("../validation/validationModel")
+const signupValidation = require("../validation/validationModel")
 
 //post request for Sign Up
 router.post("/signup", async (req, res) => {
   try {
     //validate data
-    const value = await schema.validateAsync(req.body)
+    await signupValidation(req.body)
+    const user = new User({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      password: req.body.password,
+    })
 
-    if (value) {
-      const user = new User({
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.email,
-        password: req.body.password,
-      })
-
-      const newUser = await user.save()
-      return res.status(200).json({ success: true, data: newUser })
-    }
+    const newUser = await user.save()
+    return res.status(200).json({ success: true, data: newUser })
   } catch (err) {
     res.status(400).json({ success: false, error: err.message })
   }
--- validation/validationModel.js
+++ validation/validationModel.js
@@ -1,16 +1,34 @@
 //validation
 const Joi = require("joi")
 
-const schema = Joi.object({
-  firstName: Joi.string().min(3).max(30).required(),
-  lastName: Joi.string().min(3).max(30).required(),
-  email: Joi.string()
-    .required()
-    .email({
-      minDomainSegments: 2,
-      tlds: { allow: ["com", "net"] },
-    }),
-  password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")),
-})
+//signup validation
+const signupValidation = async object => {
+  const schema = Joi.object().keys({
+    firstName: Joi.string().min(3).max(30).required(),
+    lastName: Joi.string().min(3).max(30).required(),
+    email: Joi.string()
+      .required()
+      .email({
+        minDomainSegments: 2,
+        tlds: { allow: ["com", "net"] },
+      }),
+    password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")),
+  })
 
-module.exports = schema
+  let value = schema.validate(object)
+  if (value.error) {
+    let error = value.error.details[0].message
+    // console.log(error)
+    if (error.includes("password")) {
+      throw new Error(
+        "password should be alphanumeric and should be atleast 6 characters"
+      )
+    } else {
+      throw value.error
+    }
+  } else {
+    return value
+  }
+}
+
+module.exports = signupValidation
